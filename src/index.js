4b60cd2015f2eb1ca37cbefe6fb4a8c35d86b3c4
wolfson-1 <harrywolverson@googlemail.com>
2023-03-12 17:53:35 +0000

    setup initial panel divs: Initial div layout added & setup.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-const example = document.querySelector(".content");
+import './stylesheet.css';
 
-example.innerText = "Im working";
+// setup divs for interactive windows
+// eslint-disable-next-line no-unused-vars
+const createUi = (() => {
+    const mainDiv = document.querySelector(".content");
+
+    const header = document.createElement("div");
+    header.classList.add("header");
+
+    const navPanel = document.createElement("div");
+    navPanel.classList.add("navPanel");
+
+    const mainPanel = document.createElement("div");
+    mainPanel.classList.add("mainPanel");
+
+    const footer = document.createElement("div");
+    footer.classList.add("footer");
+
+    mainDiv.appendChild(header);
+    mainDiv.appendChild(navPanel);
+    mainDiv.appendChild(mainPanel);
+    mainDiv.appendChild(footer);
+})();
+
+
+// setup interactive elements for each window
+
+
+// create array for projects & todo items
+
+// create constructors for adding new project & adding todo lists
+    // add required properties for each item
+    // add methods for adding, deleting, nessicary interaction
+    // add methods for implementing to dom
