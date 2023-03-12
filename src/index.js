8d8d9c84824668ac3c51280fda4582e19ff76a3f
wolfson-1 <harrywolverson@googlemail.com>
2023-03-12 21:24:35 +0000

    Build project constructor & setup nav div: Code initial project constructor & setup nav div for addition of projects.

--- src/index.js
+++ src/index.js
@@ -1,48 +1,99 @@
-import './stylesheet.css';
+import "./stylesheet.css";
 
-// setup divs for interactive windows
+// setup UI for todo page
 // eslint-disable-next-line no-unused-vars
 const createUi = (() => {
-    const mainDiv = document.querySelector(".content");
+  
+  // main divs
+  const mainDiv = document.querySelector(".content");
 
-    const header = document.createElement("div");
-    header.classList.add("header");
+  const header = document.createElement("div");
+  header.classList.add("header");
 
-    const navPanel = document.createElement("div");
-    navPanel.classList.add("navPanel");
+  const navPanel = document.createElement("div");
+  navPanel.classList.add("navPanel");
 
-    const mainPanel = document.createElement("div");
-    mainPanel.classList.add("mainPanel");
+  const mainPanel = document.createElement("div");
+  mainPanel.classList.add("mainPanel");
 
-    const footer = document.createElement("div");
-    footer.classList.add("footer");
+  const footer = document.createElement("div");
+  footer.classList.add("footer");
 
-    mainDiv.appendChild(header);
-    mainDiv.appendChild(navPanel);
-    mainDiv.appendChild(mainPanel);
-    mainDiv.appendChild(footer);
+  mainDiv.appendChild(header);
+  mainDiv.appendChild(navPanel);
+  mainDiv.appendChild(mainPanel);
+  mainDiv.appendChild(footer);
+
+  // nav div setup
+  const projContainer = document.createElement("div");
+  projContainer.classList.add("projContainer");
+
+  const projAddButton = document.createElement("button");
+  projAddButton.classList.add("addButton");
+  projAddButton.innerText = "Add";
+
+  navPanel.appendChild(projContainer);
+  navPanel.appendChild(projAddButton);
+
 })();
+
+// add event listeners
 
 
 // setup interactive elements for each window
 
-
 // create array for projects & todo items
-let projectsArr = [{
-        name: "shopping",
-        todo: []
-    },
-    {
-        name: "work",
-        todo: []
-    },
-    {
-        name: "this week",
-        todo: []
-    }]
+let projectsArr = [
+  {
+    name: "shopping",
+    todo: [],
+  },
+  {
+    name: "work",
+    todo: [],
+  },
+  {
+    name: "this week",
+    todo: [],
+  },
+];
 
 
 // create constructors for adding new project & adding todo lists
-    // add required properties for each item
-    // add methods for adding, deleting, nessicary interaction
-    // add methods for implementing to dom
+
+// project constructor
+const projectConstructor = (name) => {
+    
+    let todo = []
+    
+    const pushToArr = () => {
+      projectsArr.push(this);
+    };
+
+    const delFromArr = () => {
+     const myArray = projectsArr.filter((item) => item.name !== this.name);
+
+     projectsArr = myArray;
+    };
+
+    const addToDiv = () => {
+        const navPanelSelect = document.querySelector(".navPanel");
+
+        let projectDiv = document.createElement("div");
+        projectDiv.innerText = name.value;
+        navPanelSelect.appendChild(projectDiv);
+
+    };
+
+
+  return {name, todo, projectConstructor, pushToArr, delFromArr}
+}
+
+// todo constructor
+  // add required properties for each item
+  // add methods for adding, deleting, nessicary interaction
+  // add methods for implementing to dom
+
+
+
+
