ac6f9c64faa2a717938bc807204137c0e2995a6c
wolfson-1 <harrywolverson@googlemail.com>
2023-03-17 18:11:52 +0000

    finish initial add todos function by closing modal & refreshing dom

--- src/domloader.js
+++ src/domloader.js
@@ -21,90 +21,6 @@ export const removeAllChildNodes = (parent) => {
   }
 };
 
-// addTodoModal
-export const laodModal = () => {
-  const mainDiv = document.querySelector(".content");
-
-  // overlay whilst modal is open
-  createElement("div", "modalOverlay", null, mainDiv);
-
-  // create modal
-  createElement("div", "modalDiv", null, mainDiv);
-  const modalDiv = document.querySelector(".modalDiv");
-
-  // create button to close modal
-  createElement("button", "closeModal", "X", modalDiv);
-  const closeModal = document.querySelector(".closeModal");
-  closeModal.addEventListener("click", () => {
-    modalDiv.classList.remove("active");
-
-    const modalOverlay = document.querySelector(".modalOverlay");
-    modalOverlay.classList.remove("active");
-  });
-
-  // create form
-  createElement("form", "todoform", null, modalDiv);
-  const todoform = document.querySelector(".todoform");
-
-  // create title input
-  createElement("label", "todoNameLabel", "Name", todoform);
-  const todoNameLabel = (document.querySelector(".todoNameLabel").htmlFor =
-    "todoName");
-  createElement("input", "todoName", null, todoform);
-  const todoName = document.querySelector(".todoName");
-  todoName.type = "text";
-
-  // create due date
-  createElement("label", "todoDateLabel", "Due Date", todoform);
-  const todoDateLabel = (document.querySelector(".todoDateLabel").htmlFor =
-    "todoDate");
-  createElement("input", "todoDate", null, todoform);
-  const todoDate = document.querySelector(".todoDate");
-  todoDate.type = "date";
-
-  // create description
-  createElement("label", "todoDescLabel", "Description", todoform);
-  const todoDescLabel = (document.querySelector(".todoDescLabel").htmlFor =
-    "todoDesc");
-  createElement("input", "todoDesc", null, todoform);
-  const todoDesc = document.querySelector(".todoDesc");
-  todoDesc.type = "Text";
-
-  // load priority
-  createElement("label", "todoPriorityLabel", "Priority", todoform);
-  const todoPriorityLabel = (document.querySelector(".todoDescLabel").htmlFor =
-    "todoPriority");
-  createElement("input", "todoPriority", null, todoform);
-  const todoPriority = document.querySelector(".todoPriority");
-  todoPriority.type = "Text";
-
-  // add button to push new todo to relevant project
-  createElement("button", "addTodo", "Add", modalDiv);
-  const addTodo = document.querySelector(".addTodo");
-
-  addTodo.addEventListener("click", () => {
-    const mainNav =
-      document.querySelector(".mainPanel").firstChild.classList.value;
-    console.log(mainNav);
-
-    let newTodo = todoConstructor(
-      mainNav,
-      todoName.value,
-      todoDesc.value,
-      todoDate.value,
-      todoPriority.value
-    );
-
-    projectsArr.forEach((item) => {
-      if (item.name === newTodo.parentproj) {
-        item.todo.push(newTodo);
-      }
-    });
-    console.log(newTodo);
-    console.log(projectsArr);
-  });
-};
-
 // function loads todos to main panel div
 export const loadTodos = (projname) => {
   const mainNav = document.querySelector(".mainPanel");
@@ -156,6 +72,91 @@ export const navDomInteraction = () => {
       // load todo's for specific projects on click
       loadTodos(e.target.className);
     });
+  });
+};
+
+// addTodoModal
+export const laodModal = () => {
+  const mainDiv = document.querySelector(".content");
+
+  // overlay whilst modal is open
+  createElement("div", "modalOverlay", null, mainDiv);
+
+  // create modal
+  createElement("div", "modalDiv", null, mainDiv);
+  const modalDiv = document.querySelector(".modalDiv");
+
+  // create button to close modal
+  createElement("button", "closeModal", "X", modalDiv);
+  const closeModal = document.querySelector(".closeModal");
+  closeModal.addEventListener("click", () => {
+    modalDiv.classList.remove("active");
+
+    const modalOverlay = document.querySelector(".modalOverlay");
+    modalOverlay.classList.remove("active");
+  });
+
+  // create form
+  createElement("form", "todoform", null, modalDiv);
+  const todoform = document.querySelector(".todoform");
+
+  // create title input
+  createElement("label", "todoNameLabel", "Name", todoform);
+  const todoNameLabel = (document.querySelector(".todoNameLabel").htmlFor = "todoName");
+  createElement("input", "todoName", null, todoform);
+  const todoName = document.querySelector(".todoName");
+  todoName.type = "text";
+
+  // create due date
+  createElement("label", "todoDateLabel", "Due Date", todoform);
+  const todoDateLabel = (document.querySelector(".todoDateLabel").htmlFor = "todoDate");
+  createElement("input", "todoDate", null, todoform);
+  const todoDate = document.querySelector(".todoDate");
+  todoDate.type = "date";
+
+  // create description
+  createElement("label", "todoDescLabel", "Description", todoform);
+  const todoDescLabel = (document.querySelector(".todoDescLabel").htmlFor = "todoDesc");
+  createElement("input", "todoDesc", null, todoform);
+  const todoDesc = document.querySelector(".todoDesc");
+  todoDesc.type = "Text";
+
+  // load priority
+  createElement("label", "todoPriorityLabel", "Priority", todoform);
+  const todoPriorityLabel = (document.querySelector(".todoDescLabel").htmlFor = "todoPriority");
+  createElement("input", "todoPriority", null, todoform);
+  const todoPriority = document.querySelector(".todoPriority");
+  todoPriority.type = "Text";
+
+  // add button to push new todo to relevant project
+  createElement("button", "addTodo", "Add", modalDiv);
+  const addTodo = document.querySelector(".addTodo");
+
+  addTodo.addEventListener("click", () => {
+    const mainNav =
+      document.querySelector(".mainPanel").firstChild.classList.value;
+    console.log(mainNav);
+
+    let newTodo = todoConstructor(
+      mainNav,
+      todoName.value,
+      todoDesc.value,
+      todoDate.value,
+      todoPriority.value
+    );
+
+    projectsArr.forEach((item) => {
+      if (item.name === newTodo.parentproj) {
+        item.todo.push(newTodo);
+      }
+    });
+// close modal
+const modal = document.querySelector(".modalDiv").classList.remove("active");
+const modalOverlay = document.querySelector(".modalOverlay").classList.remove("active");
+// reload todos
+loadTodos();
+// reload projects (for counters)
+navDomInteraction();
   });
 };
 
